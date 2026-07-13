4a1f0ebe0c60a46dc540b4b5054daada3cbf882d
noam <noamiko173@gmail.com>
2026-07-13 23:09:00 +0300

    changes

--- src/bin/create.rs
+++ src/bin/create.rs
@@ -1,57 +1,52 @@
 use anyhow::{Context, Result};
+use memmap2::Mmap;
 use rand::distr::{Distribution, Uniform};
 use rand::seq::IndexedRandom;
+use rayon::prelude::*;
+use std::collections::HashSet;
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-pub const STATIONS: &str = "data/weather_stations.csv";
+use std::io::{BufWriter, Write};
+const COLDEST: f32 = -99.9;
+const HOTTEST: f32 = 99.9;
+const STATIONS: &str = "data/weather_stations.csv";
 const MEASUREMENTS: &str = "data/measurements.txt";
-fn get_rows() -> Result<u32> {
+fn get_rows() -> Result<usize> {
     let rows = match args().nth(1) {
-        Some(s) => s.parse::<u32>()?,
+        Some(s) => s.parse::<usize>()?,
         None => 1_000_000_000,
     };
     Ok(rows)
 }
-fn build_weather_station_name_list() -> Result<Vec<String>> {
-    let mut station_name: Vec<String> = Vec::new();
-    let file = BufReader::new(File::open(STATIONS)?);
-    for station in file.lines() {
-        let station = station?;
-        if !station.contains("#") {
-            let name = station.split(";").next().context("contanes no ';'")?;
-            station_name.push(name.into());
-        }
-    }
-    station_name.dedup();
-    Ok(station_name)
-}
-pub fn build_test_data() -> Result<()> {
-    let weather_station_names: Vec<String> = build_weather_station_name_list()?;
-    let start = std::time::Instant::now();
-    let n_rows = get_rows()?;
-    let rng = &mut rand::rng();
-    let mut file = BufWriter::new(File::create(MEASUREMENTS)?);
-    let coldest_temp: f32 = -99.9;
-    let hottest_temp: f32 = 99.9;
-    let uni = Uniform::new(coldest_temp, hottest_temp)?;
-    let station_names_10k: Vec<_> = weather_station_names
-        .sample(rng, 10_000)
-        .map(|f| {
-            let temp = uni.sample(rng);
-            format!("{};{:.1}", f, temp)
-        })
-        .collect();
-    for _ in 0..n_rows {
-        let line = station_names_10k.choose(rng).context("idk")?;
-        file.write_all(line.as_bytes())?;
-        file.write_all(b"\n")?;
-    }
-    file.flush()?;
-    println!("took {:?}", start.elapsed());
-    Ok(())
-}
 fn main() -> Result<()> {
-    build_test_data()?;
+    let start = std::time::Instant::now();
+    let rows = get_rows()?;
+    let src = File::open(STATIONS)?;
+    let mut dst = BufWriter::new(File::create(MEASUREMENTS)?);
+    let mmap = unsafe { Mmap::map(&src) }?;
+    let data: &[u8] = &mmap;
+    let uni = Uniform::new(COLDEST, HOTTEST)?;
+    let rng = &mut rand::rng();
+    let stations: Vec<&[u8]> = data
+        .par_split(|&b| b == b'\n')
+        .map(|f| f.split_inclusive(|&b| b == b';').next().unwrap_or_default())
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
+    let station_names_10k: [&[u8]; 10_000] =
+        stations.sample_array(rng).context("couldnt sample")?;
+    let nums: Vec<f32> = (0..rows)
+        .into_par_iter()
+        .map_init(
+            || rand::rng(),
+            |rng, _| (10. * uni.sample(rng)).round() / 10.,
+        )
+        .collect();
+    for num in nums {
+        dst.write_all(station_names_10k.choose(rng).context("couldnt choose")?)?;
+        writeln!(dst, "{}", num)?;
+    }
+    dst.flush()?;
+    println!("took {:.3}s", start.elapsed().as_secs_f32());
     Ok(())
 }
